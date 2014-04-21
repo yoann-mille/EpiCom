d954d5a62ad2fd826e5d4ac5e3138d092060152a
yoann-mille <yoann.mille@epitech.eu>
2014-04-21 20:57:20 +0200

    server ARM can aff pres and ready to receve signal think to install reveal !!!

---
+++ arm/public/js/presentationControl.js
@@ -0,0 +1,11 @@
+var socket = io.connect("http://localhost:3000");
+socket.on('pause', function () {
+    Reveal.togglePause();
+    console.log('pause');
+});
+
+socket.on('unpause', function () {
+    console.log('unpause');
+    if (Reveal.isPaused())
+	Reveal.togglePause();
+});
