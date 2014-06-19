1332e204f776bf4a4b704089d5b90c851644cc3c
yupin <yupin@localhost.localdomain>
2014-06-19 10:00:38 +0200

    Module VLC pour l'ARM

--- arm/libs/utils/vlc.js
+++ arm/libs/utils/vlc.js
@@ -7,7 +7,7 @@
 ** Email   <yoann.mille@epitech.eu>
 ** 
 ** Started on  Wed Jun 18 16:48:50 2014 yoann mille
-** Last update Wed Jun 18 18:00:06 2014 yoann mille
+** Last update Wed Jun 18 18:37:57 2014 yoann mille
 */
 
 var exec = require('child_process').exec;
@@ -28,7 +28,7 @@ module.exports = {
 		console.log('exec error on file : ' + files);
 		console.log(err);
 	    }
-	};
+	});
     },
 
     quit: function () {
