6be6019586dd240fd9f422a036b73894103fef74
yupin <yupin@localhost.localdomain>
2014-07-08 11:24:15 +0200

    Add header for new controller vlc

---
+++ arm/libs/utils/vlcControler.js
@@ -0,0 +1,12 @@
+/*
+** vlcControler.js for Epicom in /arm/libs/utils
+** 
+** Git project https://github.com/yoann-mille/EpiCom.git
+** 
+** Made by yoann mille
+** Email   <yoann.mille@epitech.eu>
+** 
+** Started on  Tue Jul  8 11:23:51 2014 yoann mille
+** Last update Tue Jul  8 11:24:04 2014 yoann mille
+*/
+
